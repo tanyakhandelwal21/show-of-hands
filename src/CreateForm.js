acea8df28b55f558628f303136bfccc5678a2f68
Romil Havewala <romilrh@gmail.com>
2019-02-12 17:56:11 -0500

    Modify project structure

---
+++ src/CreateForm.js
@@ -0,0 +1,23 @@
+// import React from 'react';
+// import './CreateForm.css';
+//
+//
+// class Form extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = { choices: ['Choice1', 'Choice2'] };
+//     }
+//     render() {
+//         return (
+//             <form>
+//                 <input required maxlength="40" type="text" id="title-input"
+//                 placeholder="Enter a title..."/>
+//                 <textarea required maxlength="250"
+// 				rows="4" id="description-input"
+// 				placeholder="Enter a short description..."></textarea>
+//                 // TODO: figure out how to display all choices
+//                 // TODO: implement rest of form
+//             </form>
+//         )
+//     }
+// }
