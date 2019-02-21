3744896e863e565e93c8e05516caa02d569fd7f6
jialincheoh <jcheoh@purdue.edu>
2019-02-21 00:10:38 -0500

    Back end for edit poll

--- src/selectors/polls.js
+++ src/selectors/polls.js
@@ -2,18 +2,18 @@ import moment from 'moment';
 
 // Get visible expenses
 
-export default (expenses, { text, sortBy, startDate, endDate }) => {
-  return expenses.filter((expense) => {
+export default (polls, { text, sortBy, startDate, endDate }) => {
+  return polls.filter((poll) => {
     const createdAtMoment = moment(expense.createdAt);
     const startDateMatch = startDate ? startDate.isSameOrBefore(createdAtMoment, 'day') : true;
     const endDateMatch = endDate ? endDate.isSameOrAfter(createdAtMoment, 'day') : true;
-    const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
+    const textMatch = poll.description.toLowerCase().includes(text.toLowerCase());
 
     return startDateMatch && endDateMatch && textMatch;
   }).sort((a, b) => {
     if (sortBy === 'date') {
       return a.createdAt < b.createdAt ? 1 : -1;
-    } else if (sortBy === 'amount') {
+    } else if (sortBy === 'numberOfOptions') {
       return a.amount < b.amount ? 1 : -1;
     }
   });
