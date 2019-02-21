ce44af96881075f9b7fff5d283bdad6a5d8bd341
Romil Havewala <romilrh@gmail.com>
2019-02-21 17:26:15 -0500

    touch up pages

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -6,7 +6,7 @@ export const LoginPage = ({ startLogin }) => (
   <div className="box-layout">
     <div className="box-layout__box">
       <h1 className="box-layout__title">Show Of Hands</h1>
-      <p>Create Polls and Get Votes Instantly!</p>
+      <p>Create and answer polls and surveys!</p>
       <button className="button" onClick={startLogin}>Login with Google</button>
     </div>
   </div>
