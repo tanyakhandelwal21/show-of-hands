8b59ec42f31d5c16bb42dc4ae8785f3beb73664e
Tanya Khandelwal <tkhandel@purdue.edu>
2019-02-22 08:57:18 -0500

    resolving conflict

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { startLogout } from '../actions/auth';
-<<<<<<< HEAD
-import './Header.css'
-=======
 import { NavLink } from 'react-router-dom'
->>>>>>> a183b940bf0ff1af393f100674387ae877888291
 
 export const Header = ({ startLogout, addPoll }) => (
   <header className="header">
@@ -15,9 +11,9 @@ export const Header = ({ startLogout, addPoll }) => (
         <Link className="header__title" to="/dashboard">
           <h1 className="header1">Show Of Hands</h1>
         </Link>
-        <NavLink to='dashboard/polls/add' className="is-active" exact={true}>Create Poll</NavLink>
+        <NavLink to='dashboard/polls/add' activeclassName="is-active" exact={true}> Create my poll</NavLink>
         <button className="button button--link" onClick={startLogout}>Logout</button>
-
+        
       </div>
     </div>
   </header>
@@ -25,8 +21,9 @@ export const Header = ({ startLogout, addPoll }) => (
 
 const mapDispatchToProps = (dispatch) => ({
   startLogout: () => dispatch(startLogout())
-
+  
 });
 
 
 export default connect(undefined, mapDispatchToProps)(Header);
+
