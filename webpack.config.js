9840d20da7b43932320674dfd6ceb149449425db
jialincheoh <jcheoh@purdue.edu>
2019-02-22 01:10:05 -0500

    Fix case insensitive file name

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = (env) => {
   const isProduction = env === 'production';
   const CSSExtract = new ExtractTextPlugin('styles.css');
   return {
-    entry: ['babel-polyfill', './src/app.js'],
+    entry: ['babel-polyfill', './src/App.js'],
     output: {
       path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js'
