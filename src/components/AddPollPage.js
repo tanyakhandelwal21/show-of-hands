0872c287c026768762b0ee870779ddff49b54b86
Anvi Mehta <anvimehtak@gmail.com>
2019-03-03 14:02:08 -0500

    add poll page changes

---
+++ src/components/AddPollPage.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PollForm from './PollForm';
+import { startAddPoll } from '../actions/polls';
+
+export class AddPollPage extends React.Component {
+  constructor (props) {
+    super(props);
+    this.onSubmit = this.Submit.bind(this);
+  }
+  onSubmit (poll) {
+    this.props.startAddPoll(poll);
+    this.props.history.push('/');
+  }
+  render() {
+    return (
+      <div>
+        <h1>Add Poll</h1>
+         <PollForm
+            onSubmit={this.onSubmit}
+          />
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  startAddPoll: (poll) => dispatch(startAddPoll(poll))
+});
+
+export default connect(undefined, mapDispatchToProps)(AddPollPage);
