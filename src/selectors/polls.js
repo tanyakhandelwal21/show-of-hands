954ba62531631c52381142ef0fe8cbc104341f7e
Anvi Mehta <anvimehtak@gmail.com>
2019-03-18 21:37:34 -0400

    polls.js

--- src/selectors/polls.js
+++ src/selectors/polls.js
@@ -3,17 +3,19 @@ import moment from 'moment';
 // Get visible expenses
 
 export default (polls, o) => {
+
   if(!o){
-    return polls;
+    return polls.polls;
   }
-  const { text, sortBy, startDate, endDate } = o;
-  return polls.filter((poll) => {
+
+  const { category, text, sortBy, startDate, endDate } = o;
+  return polls.polls.filter((poll) => {
     const createdAtMoment = moment(poll.start_date);
     const startDateMatch = startDate ? startDate.isSameOrBefore(createdAtMoment, 'day') : true;
     const endDateMatch = endDate ? endDate.isSameOrAfter(createdAtMoment, 'day') : true;
     const textMatch = poll.title.toLowerCase().includes(text.toLowerCase());
-
-    return startDateMatch && endDateMatch && textMatch;
+    const categoryMatch = category ? +poll.category === +category : true;
+    return startDateMatch && endDateMatch && textMatch && categoryMatch;
   }).sort((a, b) => {
     if (sortBy === 'date') {
       return a.start_date < b.start_date ? 1 : -1;
